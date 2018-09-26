c0ee6ecc43b195ece4e2f8cfa87f369c4fc3faa5
pavelusov <ipavelusov@gmail.com>
2018-09-26 23:46:26 +0500

    Use beforeEach function

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+
 import App from 'components/App';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
+let wrapper;
+
+beforeEach(()=>{
+  wrapper = shallow(<App />);
+});
+
 it('показано поле для комментария', () => {
-  const wrapper = shallow(<App />);
   expect(wrapper.find(CommentBox).length).toEqual(1);
 });
 
 it('показан список комментариев ', () => {
-  const wrapper = shallow(<App />);
   expect(wrapper.find(CommentList).length).toEqual(1);
 });
