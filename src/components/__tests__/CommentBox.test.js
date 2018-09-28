c0769024e67fb06e92c04e569cc810f0266f66e3
pavelusov <ipavelusov@gmail.com>
2018-09-28 23:39:57 +0500

    Write test for CommentBox component

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -9,11 +9,29 @@ beforeEach(() => {
   wrapper = mount(<CommentBox/>);
 });
 
-afterEach(()=>{
+afterEach(() => {
   wrapper.unmount();
 });
 
 it('показаны текстовая область и кнопка отправки формы', () => {
   expect(wrapper.find('[data-test="component-text-area"]').length).toEqual(1);
   expect(wrapper.find('[data-test="component-submit-button"]').length).toEqual(1);
+});
+
+describe('пользователь может ввести данные в текстовую область формы', () => {
+  const value = 'Новый комментарий';
+  const textArea = '[data-test="component-text-area"]';
+
+  beforeEach(()=>{
+    wrapper.find(textArea).simulate('change', { target: { value } });
+    wrapper.update();
+  });
+
+  it('состояние компонента обновленно после ввода комментария', ()=>{
+    expect(wrapper.state().comment).toEqual(value);
+  });
+
+  it('коментарий написан и отображается в текстовом поле', ()=>{
+    expect(wrapper.find(textArea).prop('value')).toEqual(value);
+  })
 });
