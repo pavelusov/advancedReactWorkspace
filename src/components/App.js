60bff543b019e664216c6b601d9f4d2b02b6ea6b
pavelusov <ipavelusov@gmail.com>
2018-09-30 23:38:03 +0500

    Write nav menu in header

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 // Application components
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
 class App extends Component {
+  renderButton = () => {
+    return (
+      <div>вход</div>
+    )
+  };
+
+  renderHeader = () => {
+    return (
+      <ul>
+        <li>
+          <Link to='/'>Главная</Link>
+        </li>
+        <li>
+          <Link to='/post'>Написать комментарий</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  };
+
   render() {
     return (
       <div>
+        {this.renderHeader()}
         <h1>Приложение "Комментарии"</h1>
         <Route path='/post' component={CommentBox} />
         <Route path='/' exact component={CommentList} />
